1f54bb8ddc14ca7fb67f033470c9b635f38a8019
Z <122657299+zulfannf@users.noreply.github.com>
2025-01-02 20:50:52 +0700

    Update to v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/zulfannf/app-say-hallo.git
 
 go 1.23.4
 
-require github.com/zulfannf/go-say-hallo v1.0.0
+require github.com/zulfannf/go-say-hallo/v2 v2.0.1 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	go_say_hallo
+
+	go_say_hallo "github.com/zulfannf/go-say-hallo/v2"
 )
 
 func main() {
-	fmt.Println(go_say_hallo.)
+	fmt.Println(go_say_hallo.SayHallo("Zoro"))
 }
